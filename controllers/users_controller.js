f920396f6e458a0d192807418222c7002a64e4da
Naveen Punia <naveen@comcodetech.com>
2023-07-02 18:37:18 +0530

    ready for passport

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,19 +3,7 @@ const user = require('../models/user');
 const { response } = require('express');
 
 module.exports.profile = (req, res) => {
-    if (req.cookies.user_id) {
-        // there is another function findById
-        user.findOne({ _id: req.cookies.user_id })
-            .then((result) => {
-                return res.render("profile", { result: result, title: `${result.name}` });
-            })
-            .catch((err) => {
-                req.cookies.user_id = null;
-                return res.redirect('/users/signin');
-            })
-    }
-    else
-        return res.redirect('/users/signin');
+    return res.render("profile", { result: result, title: `${result.name}` });
 }
 
 module.exports.posts = (req, res) => {
@@ -23,22 +11,7 @@ module.exports.posts = (req, res) => {
 }
 
 module.exports.signin = (req, res) => {
-    user.findOne({ email: req.body.email })
-        .then((result) => {
-            if (result) {
-                if (result.password == req.body.password) {
-                    res.cookie("user_id", result._id);
-                    // creating the session 
-                    return res.redirect('/users/profile');
-                }
-                else return res.redirect('back');
-            }
-            else return res.redirect('back');
-        })
-        .catch((err) => {
-            console.log('error duing signing in !');
-            return res.redirect('/users/signin');
-        })
+
 }
 
 
