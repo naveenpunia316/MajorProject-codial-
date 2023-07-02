0fa59c4889020dddce9c35cac01ad7e7c32334e4
Naveen Punia <naveenpunia316@gmail.com>
2023-07-02 07:38:34 +0530

    added express layouts

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const port = 8000;
 const app = express();
 const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
+app.use(expressLayouts); // tell our app to use it
+
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+// extract style and scripts from sub pages into layout
+// use this middle ware before the routes 
 app.use('/', require('./routes/index'));
 // index will be found automatically 
 
